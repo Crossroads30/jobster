d6b4ba5511797dbaa15a2541f9b99a4bc137b229
crossroads30 <crossroads30@mail.ru>
2024-01-23 23:15:28 +0300

    feat: add thunk with action for login/register

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,6 +6,19 @@ const initialState = {
 	user: null,
 }
 
+export const registerUser = createAsyncThunk(
+  'user/registerUser', // first: name of slice/ second: name of action
+  async (user, thunkAPI) => {
+    console.log(`Register User: ${JSON.stringify(user)}`)
+  }
+)
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (user, thunkAPI) => {
+    console.log(`Login User: ${JSON.stringify(user)}`)
+  }
+)
+
 const userSlice = createSlice({
 	name: 'user',
 	initialState,
