42bfe46fd57fd125449aa925324c6cf9db61692f
crossroads30 <crossroads30@mail.ru>
2024-01-23 17:41:58 +0300

    fix: rename

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,39 @@
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Button, Flex } from 'antd'
+import { Logo } from '../components'
+
+const LandingPage = () => {
+	return (
+		<Wrapper>
+			<nav>
+				<Logo />
+			</nav>
+			{/* <div className='container page'> */}
+			<Flex
+				wrap='wrap'
+				align='center'
+				justify='space-between'
+				className='container page'
+			>
+				<div className='info'>
+					<h1>
+						job <span>tracking</span> app
+					</h1>
+					<p>
+						Master cleanse edison bulb jawn pinterest. Lomo next level blue
+						bottle austin. Chartreuse iceland 90's food truck, vice schlitz
+						leggings. Ennui cliche JOMO forage.
+					</p>
+					<Button type='primary' style={{ background: 'var(--primary-500)' }}>
+						Login/Register
+					</Button>
+				</div>
+				<img src={main} alt='job hunt' className='img main-img' />
+			</Flex>
+			{/* </div> */}
+		</Wrapper>
+	)
+}
+
+export default LandingPage
