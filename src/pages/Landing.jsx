9ea2e9aa2737112c62ab516d19f66fc036aed243
crossroads30 <crossroads30@mail.ru>
2024-01-23 17:22:10 +0300

    refactor: import styles from landingPage.js

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
-import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
 import { Button, Flex } from 'antd'
 
 const Landing = () => {
@@ -30,43 +30,5 @@ const Landing = () => {
 		</Wrapper>
 	)
 }
-
-const Wrapper = styled.main`
-	nav {
-		width: var(--fluid-width);
-		max-width: var(--max-width);
-		margin: 0 auto;
-		height: var(--nav-height);
-		display: flex;
-		align-items: center;
-	}
-	.page {
-		min-height: calc(100vh - var(--nav-height));
-		margin-top: -3rem;
-	}
-	h1 {
-		font-weight: 700;
-		span {
-			color: var(--primary-500);
-		}
-	}
-	p {
-		color: var(--grey-600);
-	}
-	.main-img {
-		display: none;
-	}
-  @media (min-width: 992px) {
-.page {
-  flex-wrap: nowrap;
-  gap: 3rem;
-}
-.main-img {
-  display: block;
-  width: 450px;
-  gap: 3rem;
-}
-  }
-`
 
 export default Landing
