672ecb61bb0689c1027af961233bfc0a429d577d
crossroads30 <crossroads30@mail.ru>
2024-01-23 19:18:03 +0300

    feat: set structure

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,17 @@
+import Wrapper from '../assets/wrappers/ErrorPage'
+import image from '../assets/images/not-found.svg'
+import { Link } from 'react-router-dom'
+
 const ErrorPage = () => {
-  return (
-    <div>ErrorPage</div>
-  )
+	return (
+		<Wrapper className='full-page'>
+			<div>
+				<img src={image} alt='not-found' />
+				<h3>ohh! page not found</h3>
+				<p>We can't seem to find the page you're looking for</p>
+				<Link to='/'>back home</Link>
+			</div>
+		</Wrapper>
+	)
 }
 export default ErrorPage
