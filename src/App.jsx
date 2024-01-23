e7cfc4911e2866207d2c72f9a6518c274261926e
crossroads30 <crossroads30@mail.ru>
2024-01-23 18:59:59 +0300

    feat: add browser router with all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LandingPage, DashboardPage, ErrorPage, RegisterPage } from './pages'
 
 const App = () => {
 	return (
-		<div>
-			<LandingPage />
-      <ErrorPage />
-      <DashboardPage />
-      <RegisterPage />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<DashboardPage />} />
+				<Route path='/landing' element={<LandingPage />} />
+				<Route path='/register' element={<RegisterPage />} />
+				<Route path='*' element={<ErrorPage />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 export default App
