a32234eb129cbab147efd0b430751e6a53f64188
crossroads30 <crossroads30@mail.ru>
2024-01-24 15:09:30 +0300

    feat: add try catch/ builder for login

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -19,11 +19,17 @@ export const registerUser = createAsyncThunk(
 			return thunkAPI.rejectWithValue(error.response.data.msg)
 		}
 	}
-)
+) 
 export const loginUser = createAsyncThunk(
-	'user/loginUser',
+	'user/loginUser', // first: name of slice/ second: name of action
 	async (user, thunkAPI) => {
-		// console.log(`Login User: ${JSON.stringify(user)}`)
+		try {
+			const response = await customFetch.post('/auth/login', user) // await!!!
+			// console.log(response)
+			return response.data
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error.response.data.msg)
+		}
 	}
 )
 
@@ -32,6 +38,7 @@ const userSlice = createSlice({
 	initialState,
 	extraReducers: builder => {
 		builder
+			// registerUser
 			.addCase(registerUser.pending, state => {
 				state.isLoading = true
 			})
@@ -44,9 +51,24 @@ const userSlice = createSlice({
 			.addCase(registerUser.rejected, (state, { payload }) => {
 				state.isLoading = false
 				toast.error(payload)
-				// console.log(payload)
+			})
+			// loginUser
+			.addCase(loginUser.pending, state => {
+				state.isLoading = true
+			})
+			.addCase(loginUser.fulfilled, (state, { payload }) => {
+				const { user } = payload
+				state.isLoading = false
+				state.user = user
+				toast.success(`welcome back, ${user.name}`)
+			})
+			.addCase(loginUser.rejected, (state, { payload }) => {
+				state.isLoading = false
+				toast.error(payload)
 			})
 	},
+
+
 })
 
 export default userSlice.reducer
