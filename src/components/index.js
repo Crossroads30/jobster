a5466a7e23a8d76b36e2b9fe6d14d3748ec4da93
crossroads30 <crossroads30@mail.ru>
2024-01-24 17:07:47 +0300

    feat: add imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Logo from '../components/Logo'
 import FormRow from '../components/FormRow'
+import BigSidebar from './BigSidebar'
+import SmallSideBar from './SmallSideBar'
+import Navbar from './Navbar'
 
-export { Logo, FormRow }
+
+export { Logo, FormRow, BigSidebar, SmallSideBar, Navbar }
