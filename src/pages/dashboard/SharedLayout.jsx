a9e5f2cc3ff7b8e9fb05a6e8db8040ee0de69ead
crossroads30 <crossroads30@mail.ru>
2024-01-24 17:08:34 +0300

    feat: add structure with outlet

--- src/pages/dashboard/SharedLayout.jsx
+++ src/pages/dashboard/SharedLayout.jsx
@@ -1,6 +1,19 @@
+import { Outlet } from 'react-router-dom'
+import { Navbar, BigSidebar, SmallSideBar } from '../../components'
+import Wrapper from '../../assets/wrappers/SharedLayout'
+
 const SharedLayout = () => {
-  return (
-    <div>SharedLayout</div>
-  )
+	return <Wrapper>
+    <main className='dashboard'>
+      <SmallSideBar />
+      <BigSidebar />
+      <div>
+        <Navbar />
+        <div className="dashboard-page">
+          <Outlet />
+        </div>
+      </div>
+    </main>
+  </Wrapper>
 }
 export default SharedLayout
