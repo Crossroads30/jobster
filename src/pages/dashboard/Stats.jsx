22288c4d0d02f44a3d86e52bdae51788755c7572
crossroads30 <crossroads30@mail.ru>
2024-01-28 20:35:22 +0300

    feat: add components

--- src/pages/dashboard/Stats.jsx
+++ src/pages/dashboard/Stats.jsx
@@ -1,15 +1,20 @@
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import { showStats } from "../../features/allJobs/allJobsSlice"
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { showStats } from '../../features/allJobs/allJobsSlice'
+import { StatsContainer, ChartsContainer } from '../../components'
 
 const Stats = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(showStats())
-  }, [])
+	const { isLoading, monthlyApplications } = useSelector(store => store.allJobs)
+	const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(showStats())
+	}, [])
 
-  return (
-    <div>Stats</div>
-  )
+	return (
+		<>
+			<StatsContainer />
+			{monthlyApplications.length > 0 && <ChartsContainer />}
+		</>
+	)
 }
 export default Stats
