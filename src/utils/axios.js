c85b27cb87949b32d6b7b0cfba1c4dcf95f1734c
crossroads30 <crossroads30@mail.ru>
2024-01-28 18:18:49 +0300

    feat: add interceptors

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,20 @@
-import axios from "axios";
+import axios from 'axios'
+import { getUserFromLocalStorage } from './localStorage'
 
 const customFetch = axios.create({
 	baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
 })
+
+// response interceptors
+customFetch.interceptors.request.use(
+	config => {
+		const user = getUserFromLocalStorage()
+		config.headers['Authorization'] = `Bearer ${user.token}`
+		return config
+	},
+	error => {
+		return Promise.reject(error)
+	}
+)
 
 export default customFetch
