5724b39d82706b3a0fddb0cfc94d1ee2621c7bda
crossroads30 <crossroads30@mail.ru>
2024-01-28 21:13:40 +0300

    feat: set comp structure

--- src/components/StatItem.jsx
+++ src/components/StatItem.jsx
@@ -1,6 +1,14 @@
-const StatItem = () => {
+import Wrapper from '../assets/wrappers/StatItem.js'
+
+const StatItem = ({count, title, icon, color,bcg}) => {
   return (
-    <h2>StatItem</h2>
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className='count'>{count}</span>
+        <span className='icon'>{icon}</span>
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
   )
 }
 export default StatItem
