c78ceebccbbadf5c57e2f8f2efd2dd20d72028f1
crossroads30 <crossroads30@mail.ru>
2024-01-28 18:20:00 +0300

    refactor: remove header, add comments

--- src/features/job/jobThunk.js
+++ src/features/job/jobThunk.js
@@ -1,15 +1,26 @@
+import { authHeader } from '../../utils/authHeader'
 import customFetch from '../../utils/axios'
 import { getAllJobs, hideLoading, showLoading } from '../allJobs/allJobsSlice'
 import { logoutUser } from '../user/userSlice'
 import { clearJobForm } from './job.Slice'
 
+// Way1 - we can pass headers as separate function from this file:
+// const authHeader = thunkAPI => {
+// 	return {
+// 		headers: {
+// 			authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+// 		},
+// 	}}
+
+//Way2 - we can pass headers as separate function from other file(authHeader.js)
+
+//Way3 - we can set it in customFetch as axios interceptors
+//for way3 we must remove all 'headers' from thunks as we set it in axios
+
 export const createJobThunk = async (job, thunkAPI) => {
 	try {
-		const response = await customFetch.post('/jobs', job, {
-			headers: {
-				authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-			},
-		})
+		// Way1
+		const response = await customFetch.post('/jobs', job)
 		thunkAPI.dispatch(clearJobForm())
 		return response.data
 	} catch (error) {
@@ -23,11 +34,7 @@ export const createJobThunk = async (job, thunkAPI) => {
 export const deleteJobThunk = async (jobId, thunkAPI) => {
 	thunkAPI.dispatch(showLoading()) // we show loading from allJobsSlice
 	try {
-		const response = await customFetch.delete(`/jobs/${jobId}`, {
-			headers: {
-				authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-			},
-		})
+		const response = await customFetch.delete(`/jobs/${jobId}`)
 		thunkAPI.dispatch(getAllJobs()) // we again get all jobs from server
 		return response.data.msg // this data will be passed as payload to toast for success msg
 	} catch (error) {
@@ -38,14 +45,29 @@ export const deleteJobThunk = async (jobId, thunkAPI) => {
 
 export const editJobThunk = async ({ jobId, job }, thunkAPI) => {
 	try {
-		const response = await customFetch.patch(`/jobs/${jobId}`, job, {
-			headers: {
-				authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-			},
-		})
+		const response = await customFetch.patch(`/jobs/${jobId}`, job)
 		thunkAPI.dispatch(clearJobForm())
 		return response.data
 	} catch (error) {
 		return thunkAPI.rejectWithValue(error.response.data.msg)
 	}
 }
+
+
+// example with headers in thunk
+// export const createJobThunk = async (job, thunkAPI) => {
+// 	try {
+// 		const response = await customFetch.post('/jobs', job, {
+// 			headers: {
+// 				authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+// 			},
+// 		})
+// 		thunkAPI.dispatch(clearJobForm())
+// 		return response.data
+// 	} catch (error) {
+// 		if (error.response.status === 401) {
+// 			thunkAPI.dispatch(logoutUser())
+// 			return thunkAPI.rejectWithValue('Unauthorized! Logging out...')
+// 		}
+// 	}
+// }
