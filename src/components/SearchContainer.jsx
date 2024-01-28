449a2ad085496efa064d26b9a2e9facf90e6b886
crossroads30 <crossroads30@mail.ru>
2024-01-28 13:57:52 +0300

    feat: add starting structure

---
+++ src/components/SearchContainer.jsx
@@ -0,0 +1,14 @@
+import Wrapper from '../assets/wrappers/SearchContainer'
+
+const SearchContainer = () => {
+  return (
+    <Wrapper>
+      <form className="form">
+        <h3>all jobs</h3>
+        <div className="form-center"> 
+        </div>
+      </form>
+    </Wrapper>
+  )
+}
+export default SearchContainer
