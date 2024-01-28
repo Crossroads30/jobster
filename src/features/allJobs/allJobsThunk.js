e5d2d2a03ae8349be76a07bf32df661ab7898736
crossroads30 <crossroads30@mail.ru>
2024-01-28 20:19:07 +0300

    feat: add thunk for show stats

--- src/features/allJobs/allJobsThunk.js
+++ src/features/allJobs/allJobsThunk.js
@@ -1,11 +1,23 @@
 import customFetch from '../../utils/axios'
 
-export const getAllJobsThunk = async (url, _, thunkAPI) => {
+export const getAllJobsThunk = async (_, thunkAPI) => {
+	let url = `/jobs`
 	try {
 		const response = await customFetch.get(url)
 		// console.log(response.data)
 		return response.data
 	} catch (error) {
+		return thunkAPI.rejectWithValue(error.response.data.msg)
+	}
+}
+
+export const showStatsThunk = async (_, thunkAPI) => {
+	try {
+		const response = await customFetch.get('/jobs/stats')
+		// console.log(response.data)
+		return response.data
+	} catch (error) {
+		console.log(error)
 		return thunkAPI.rejectWithValue(error.response.data.msg)
 	}
 }
