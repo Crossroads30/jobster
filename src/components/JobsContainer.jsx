da011fac69992ec2894ea884ac16c87974245b21
crossroads30 <crossroads30@mail.ru>
2024-01-28 14:53:30 +0300

    feat: add useEffect for displaying all jobs

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -2,34 +2,36 @@ import { useDispatch, useSelector } from 'react-redux'
 import Wrapper from '../assets/wrappers/JobsContainer'
 import Job from './Job'
 import Loading from './Loading'
+import { getAllJobs } from '../features/allJobs/allJobsSlice'
+import { useEffect } from 'react'
 
 const JobsContainer = () => {
 	const { jobs, isLoading } = useSelector(store => store.allJobs)
-  const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-  if(isLoading) {
-    return(
-      <Loading center/>
-    )
-  }
-  if(jobs.length === 0) {
-    return(
-      <Wrapper>
-        <h2>No jobs to display...</h2>
-      </Wrapper>
-    )
-  }
+	useEffect(() => {
+		dispatch(getAllJobs())
+	}, [])
+
+	if (isLoading) {
+		return <Loading center />
+	}
+	if (jobs.length === 0) {
+		return (
+			<Wrapper>
+				<h2>No jobs to display...</h2>
+			</Wrapper>
+		)
+	}
 
 	return (
 		<Wrapper>
-    <h5>jobs info</h5>
+			<h5>jobs info</h5>
 			<div className='jobs'>
-        {jobs.map(job => {
-          return (
-            <Job key={job._id} {...job} />
-          )
-        })}
-      </div>
+				{jobs.map(job => {
+					return <Job key={job._id} {...job} />
+				})}
+			</div>
 		</Wrapper>
 	)
 }
