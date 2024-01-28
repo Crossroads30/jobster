f062395ccef51b8a0d3b1f6b591dd85d96485dc6
crossroads30 <crossroads30@mail.ru>
2024-01-28 17:22:26 +0300

    feat: add edit thunk

--- src/features/job/jobThunk.js
+++ src/features/job/jobThunk.js
@@ -14,7 +14,6 @@ export const createJobThunk = async (url, job, thunkAPI) => {
 		return response.data
 	} catch (error) {
 		if (error.response.status === 401) {
-			thunkAPI.dispatch(setAuthError())
 			thunkAPI.dispatch(logoutUser())
 			return thunkAPI.rejectWithValue('Unauthorized! Logging out...')
 		}
@@ -33,6 +32,20 @@ export const deleteJobThunk = async (url, thunkAPI) => {
 		return response.data.msg // this data will be passed as payload to toast for success msg 
 	} catch (error) {
 		thunkAPI.dispatch(hideLoading()) // if an error we hide loading from allJobsSlice
+		return thunkAPI.rejectWithValue(error.response.data.msg)
+	}
+}
+
+export const editJobThunk = async (url, job, thunkAPI) => {
+	try {
+		const response = await customFetch.patch(url, job, {
+			headers: {
+				authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+			},
+		})
+		thunkAPI.dispatch(clearJobForm())
+		return response.data
+	} catch (error) {
 		return thunkAPI.rejectWithValue(error.response.data.msg)
 	}
 }
