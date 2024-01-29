1f02cd8780a5fa2081636ffb760d89819125b364
crossroads30 <crossroads30@mail.ru>
2024-01-29 11:15:46 +0300

    fix: rename components

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
-import BarChart from './BarChart'
-import AreaChart from './AreaChart'
+import BartChartComponent from './BarChart'
+import AreaChartComponent from './AreaChart'
 import Wrapper from '../assets/wrappers/ChartsContainer'
 import { useSelector } from 'react-redux'
 import useSelection from 'antd/es/table/hooks/useSelection'
@@ -9,12 +9,18 @@ import useSelection from 'antd/es/table/hooks/useSelection'
 const ChartsContainer = () => {
 	const [barChart, setBarChart] = useState(true)
 	const { monthlyApplications: data } = useSelector(store => store.allJobs)
-	return <Wrapper>
-    <h4>monthly applications</h4>
-    <button type="button" onClick={()=> setBarChart(!barChart)}>
-      {barChart ? 'area chart' : 'bar chart'}
-    </button>
-    {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
-  </Wrapper>
+	return (
+		<Wrapper>
+			<h4>monthly applications</h4>
+			<button type='button' onClick={() => setBarChart(!barChart)}>
+				{barChart ? 'area chart' : 'bar chart'}
+			</button>
+			{barChart ? (
+				<BartChartComponent data={data} />
+			) : (
+				<AreaChartComponent data={data} />
+			)}
+		</Wrapper>
+	)
 }
 export default ChartsContainer
