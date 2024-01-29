ac1aa661b2e7e7220e75920f149ec17e270c00ef
crossroads30 <crossroads30@mail.ru>
2024-01-29 10:26:56 +0300

    feat: add cond for comp to be shown

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -1,6 +1,20 @@
+import React, { useState } from 'react'
+
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+import { useSelector } from 'react-redux'
+import useSelection from 'antd/es/table/hooks/useSelection'
+
 const ChartsContainer = () => {
-  return (
-    <h2>ChartsContainer</h2>
-  )
+	const [barChart, setBarChart] = useState(true)
+	const { monthlyApplications: data } = useSelector(store => store.allJobs)
+	return <Wrapper>
+    <h4>monthly applications</h4>
+    <button type="button" onClick={()=> setBarChart(!barChart)}>
+      {barChart ? 'area chart' : 'bar chart'}
+    </button>
+    {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+  </Wrapper>
 }
 export default ChartsContainer
